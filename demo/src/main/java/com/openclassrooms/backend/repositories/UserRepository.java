20b46a0813ff357d74ad13f485d54eafa5236a78
Emma Liefmann <60130428+emmaliefmann@users.noreply.github.com>
2024-12-20 14:38:26 +0100

    correction password encrypt, methode verification mdp (#6)

--- demo/src/main/java/com/openclassrooms/backend/repositories/UserRepository.java
+++ demo/src/main/java/com/openclassrooms/backend/repositories/UserRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
   Optional<User> findUserById(Long id);
+
+  Optional<User> findByEmail(String email);
 }
